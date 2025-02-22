720df0b20027e434421f15db3d2cbcd1a8f2d278
Brenden Moraless <brenden@moralcompute.com>
2025-02-22 01:29:28 -0600

    fighting with install stuff, I don't like this

--- snap.py
+++ snap.py
@@ -1,8 +1,12 @@
 from picamera2 import Picamera2, Preview
 import time
+Picamera2.set_logging(Picamera2.ERROR)
 picam2 = Picamera2()
-camera_config = picam2.create_preview_configuration()
+picam2.set_logging(Picamera2.ERROR)
+camera_config = picam2.create_still_configuration()
 picam2.configure(camera_config)
-picam2.start_preview(Preview.NULL)
 picam2.start()
-picam2.capture_file("test.jpg")
+picam2.capture_file("image.jpg")
+print("OHHHH SHIT")
+print("HOW ABOUT NOW?")
+print("asdf?")
