b23894cbb0b9c280b1ade4e8426d56e08097bf3f
Brenden Moraless <brenden@moralcompute.com>
2025-02-22 01:50:31 -0600

    pushing camera configs and parsing them

--- snap.py
+++ snap.py
@@ -1,5 +1,25 @@
-from picamera2 import Picamera2, Preview
-import time
+from picamera2 import Picamera2
+from yaml import safe_load, YAMLError
+import socket
+
+host_name = socket.gethostname()
+
+camera_config = None
+with open("camera_configs.yaml") as stream:
+    try:
+        configs = safe_load(stream)
+        for config in configs:
+            if config['host'] == host_name:
+                camera_config = config
+    except YAMLError as exc:
+        print(exc)
+
+if camera_config is None:
+    raise ValueError("No config found for {}".format(host_name));
+
+print(camera_config)
+
+
 Picamera2.set_logging(Picamera2.ERROR)
 picam2 = Picamera2()
 picam2.set_logging(Picamera2.ERROR)
