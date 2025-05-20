bfc97ffd6803e0f59e67364d46d681072f043545
brenden <brenden@moralcompute.com>
2025-05-19 19:43:14 -0500

    ugh

--- view.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-img = np.load("captures/picam0.local/projector_x_coords.npy")
-plt.imshow(img, cmap="jet")
-plt.title("Decoded projector X")
-plt.colorbar()
-plt.show()
